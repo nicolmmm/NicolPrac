813fae8494665fa09bc8892edb075c96cb514155
Nicol Mouton <nicolmouton@yahoo.com>
2021-08-28 10:34:30 +1000

    initial commit nextjs + tailwind

---
+++ README.md
@@ -0,0 +1,3 @@
+# Un techo para mi club (A roof for my club)
+
+A foundrising site for a great cause
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <div className="container">
+        <h2 className="text-lg text-center">New Project</h2>
+      </div>
+    </div>
+  )
+}
